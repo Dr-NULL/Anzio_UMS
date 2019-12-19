47979a632781d1b06853321dacd9173ac66d4dc2
Dr-NULL <silvaaguilarfelipe@gmail.com>
2019-12-19 11:58:23 -0300

    Fix CSS Menu

--- client/src/app/configuration/app.routing.ts
+++ client/src/app/configuration/app.routing.ts
@@ -5,10 +5,12 @@ export { RouterModule };
 import { MainComponent } from '../components/pages/main/main.component';
 import { SetupComponent } from '../components/pages/setup/setup.component';
 import { LoginComponent } from '../components/pages/login/login.component';
+import { AddComponent } from '../components/pages/sistema/add/add.component';
 
 // Configurar rutas aquí
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'setup', component: SetupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'sistema/add', component: AddComponent }
 ];
