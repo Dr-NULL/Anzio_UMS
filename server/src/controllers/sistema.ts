3a035447c5223ede99fe6ea348ade804f2144cca
Dr-NULL <silvaaguilarfelipe@gmail.com>
2019-12-24 13:10:11 -0300

    Endpoint created

--- server/src/controllers/sistema.ts
+++ server/src/controllers/sistema.ts
@@ -37,7 +37,7 @@ getAll.callback = async (req, res) => {
 export const add = new EndPoint()
 add.method = "post"
 add.path = "/sistema/add"
-add.callback = async (req, res) => {    
+add.callback = async (req, res) => {
     try {
         if (req.session.isCreated) {
             // Get User
@@ -88,3 +88,46 @@ add.callback = async (req, res) => {
 export const kill = new EndPoint()
 kill.method = "get"
 kill.path = "/sistema/kill/:id"
+kill.callback = async (req, res) => {
+    try {
+        if (req.session.isCreated) {
+            // Get User
+            const user = await Usuario.findOne({
+                id: req.session.data.id
+            })
+
+            // Validar permisos
+            if ((!user.isSystem) && (!user.isAdmin)) {
+                res.api.failed({
+                    HttpResponse: StatusCodes.cod403,
+                    details: "Usted no posee los permisos suficientes para agregar sistemas."
+                })
+
+            } else {
+                const sist = await Sistema.findOne({
+                    id: parseInt(req.params.id)
+                })
+
+                if (sist == null) {
+                    // El sistema no existe
+                    res.api.failed({
+                        HttpResponse: StatusCodes.cod404,
+                        details: "El sistema especificado no existe."
+                    })
+                } else {
+                    // Eliminar el sistema
+                    await sist.remove()
+                    res.api.send()
+                }
+            }
+        } else {
+            res.api.failed({
+                HttpResponse: StatusCodes.cod401,
+                details: "Credenciales de acceso incorrectas, reintente..."
+            })
+        }
+    } catch (err) {
+        console.log(err)
+        res.api.catch(err)
+    }
+}
